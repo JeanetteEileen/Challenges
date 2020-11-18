84c0cd7a8c43a468ebf94a6ff59b808ef52233e9
JenaetteEileen <jeanetteeileen787@gmail.com>
2020-11-17 20:31:48 -0500

    challenges

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("01_weekOneChallenges")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("01_weekOneChallenges")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7bc40b0a-8710-4af4-85be-b31899a526e0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ weekOneChallenges.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace _01_weekOneChallenges
+{
+    [TestClass]
+    public class WeekOneChallenges
+    {
+        [TestMethod]
+        public void SetfirstNameLastNameAge()
+        {
+            // declare and initialize first name last name and age
+            string firstName = "Jeanette";
+            string lastName = "Schriner";
+            int age = 59;
+            int sum = age + 7;
+            Console.WriteLine(sum);
+            int diff = age - 7;
+            Console.WriteLine(diff);
+            int prod = age * 7;
+            Console.WriteLine(prod);
+            decimal quot = age / 7;
+            Console.WriteLine(quot);
+            int remain = age % 7;
+            Console.WriteLine(remain);
+        }
+        [TestMethod]
+        public void MyMovieList ()
+        {
+                //declare and initialize array that holds a collection of 4 or more favorite books, or movies
+
+                string[] movies = { "It Happened on 5th Ave", "Holiday Inn", "Die Hard", "The Cowboys" };
+               
+        }
+            [TestMethod]
+            public void ListofDates()
+            {
+                //make a list to hold dates include the current date and time
+                List<DateTime> listofDates = new List<DateTime>();
+                listofDates.Add(DateTime.Now);
+                listofDates.Add(new DateTime(2020, 12, 25));
+                listofDates.Add(new DateTime(2020, 11, 26));
+                listofDates.Add(new DateTime(2020, 12, 31));
+            }
+            [TestMethod]
+            public void Sleepmethod()
+            {
+            Console.WriteLine("How many hours did you sleep last night?");
+            int sleep = 7;
+            if (sleep >= 10)
+            {
+                Console.WriteLine("Wow that's a lot of sleep");
+            }
+            else if (sleep >= 8 && sleep < 10)
+            {
+                Console.WriteLine("You should be pretty rested.");
+            }
+            else if (sleep > 4 && sleep < 8)
+            {
+                Console.WriteLine("Bummer");
+            }
+            else
+            {
+                Console.WriteLine("Oh man, get some sleep!");
+            }
+            }
+            [TestMethod]
+            public void SwitchCase()
+            {
+                // switch case
+                Console.WriteLine("Rate your date :(, bad, ok, good, greeat ");
+            string mood = Console.ReadLine();
+                switch (mood)
+                {
+                    case "great":
+                        Console.WriteLine("Wow you are having a great day");
+                        break;
+                    case "good":
+                        Console.WriteLine("Glad your day is good");
+                        break;
+                    case "ok":
+                        Console.WriteLine("Your day is: Okay");
+                        break;
+                    case "bad":
+                        Console.WriteLine("Your day is: Bad");
+                        break;
+                    case ":(":
+                        Console.WriteLine("Your day is: :( Sorryse");
+                        break;
+                }
+            }
+            [TestMethod]
+            public void Supercalifragilisticexpialidocious()
+            {
+                //Supercalifragilisticexpialidocious written 1 letter at a time
+
+                string supern = "Supercalifragilisticexpialidocious";
+                foreach (char letter in supern)
+                {
+                    Console.WriteLine(letter);
+                }
+            }
+            [TestMethod]
+            public void INotIandL()
+            {
+                string[] supern1 = { "S", "u", "p", "e", "r", "c", "a", "l", "i", "f", "r", "a", "g", "i", "l", "i", "s", "t", "i", "c", "e", "x", "p", "i", "a", "l", "i", "d", "o", "c", "i", "o", "u", "s" };
+
+                for (int index = 0; index < 34; index++)
+                {
+                if (supern1[index] == "i")
+                {
+                    Console.WriteLine("i");
+                }
+                {
+                    Console.WriteLine("Not an i");
+                }
+                    if (supern1[index] == "L")
+                        Console.WriteLine("L");
+                }
+            }
+        }
+    }
+
+
+
+
+
+ 
