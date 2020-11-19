dbeff0693879c901807e3c8e30f0bec162771391
JenaetteEileen <jeanetteeileen787@gmail.com>
2020-11-19 15:02:33 -0500

    updated week on challenges

--- weekOneChallenges.cs
+++ weekOneChallenges.cs
@@ -26,26 +26,27 @@ namespace _01_weekOneChallenges
             Console.WriteLine(remain);
         }
         [TestMethod]
-        public void MyMovieList ()
+        public void MyMovieList()
         {
-                //declare and initialize array that holds a collection of 4 or more favorite books, or movies
+            //declare and initialize array that holds a collection of 4 or more favorite books, or movies
 
-                string[] movies = { "It Happened on 5th Ave", "Holiday Inn", "Die Hard", "The Cowboys" };
-               
+            string[] movies = { "It Happened on 5th Ave", "Holiday Inn", "Die Hard", "The Cowboys" };
+
         }
-            [TestMethod]
-            public void ListofDates()
-            {
-                //make a list to hold dates include the current date and time
-                List<DateTime> listofDates = new List<DateTime>();
-                listofDates.Add(DateTime.Now);
-                listofDates.Add(new DateTime(2020, 12, 25));
-                listofDates.Add(new DateTime(2020, 11, 26));
-                listofDates.Add(new DateTime(2020, 12, 31));
-            }
-            [TestMethod]
-            public void Sleepmethod()
-            {
+        [TestMethod]
+        public void ListofDates()
+        {
+            //make a list to hold dates include the current date and time
+            List<DateTime> listofDates = new List<DateTime>();
+            DateTime today = DateTime.Now;
+            listofDates.Add(today);
+            listofDates.Add(new DateTime(2020, 12, 25));
+            listofDates.Add(new DateTime(2020, 11, 26));
+            listofDates.Add(new DateTime(2020, 12, 31));
+        }
+        [TestMethod]
+        public void HowMuchSleepmethod()
+        {
             Console.WriteLine("How many hours did you sleep last night?");
             int sleep = 7;
             if (sleep >= 10)
@@ -64,63 +65,68 @@ namespace _01_weekOneChallenges
             {
                 Console.WriteLine("Oh man, get some sleep!");
             }
-            }
-            [TestMethod]
-            public void SwitchCase()
-            {
-                // switch case
-                Console.WriteLine("Rate your date :(, bad, ok, good, greeat ");
-            string mood = Console.ReadLine();
-                switch (mood)
-                {
-                    case "great":
-                        Console.WriteLine("Wow you are having a great day");
-                        break;
-                    case "good":
-                        Console.WriteLine("Glad your day is good");
-                        break;
-                    case "ok":
-                        Console.WriteLine("Your day is: Okay");
-                        break;
-                    case "bad":
-                        Console.WriteLine("Your day is: Bad");
-                        break;
-                    case ":(":
-                        Console.WriteLine("Your day is: :( Sorryse");
-                        break;
-                }
-            }
-            [TestMethod]
-            public void Supercalifragilisticexpialidocious()
-            {
-                //Supercalifragilisticexpialidocious written 1 letter at a time
+        }
+        [TestMethod]
+        public void SwitchCaseHowWasYourDay()
+        {
+            // switch case
 
-                string supern = "Supercalifragilisticexpialidocious";
-                foreach (char letter in supern)
-                {
-                    Console.WriteLine(letter);
-                }
-            }
-            [TestMethod]
-            public void INotIandL()
+            string mood = "okay";
+            switch (mood)
             {
-                string[] supern1 = { "S", "u", "p", "e", "r", "c", "a", "l", "i", "f", "r", "a", "g", "i", "l", "i", "s", "t", "i", "c", "e", "x", "p", "i", "a", "l", "i", "d", "o", "c", "i", "o", "u", "s" };
+                case "great":
+                    Console.WriteLine("Wow you are having a great day");
+                    break;
+                case "good":
+                    Console.WriteLine("Glad your day is good");
+                    break;
+                case "ok":
+                    Console.WriteLine("Your day is Okay");
+                    break;
+                case "bad":
+                    Console.WriteLine("Sorry your day is bad");
+                    break;
+                case ":(":
+                    Console.WriteLine("Hope your day gets better :(");
+                    break;
+            }
+        }
+        [TestMethod]
+        public void Supercalifragilisticexpialidocious()
+        {
+            //Supercalifragilisticexpialidocious written 1 letter at a time
 
-                for (int index = 0; index < 34; index++)
-                {
+            string supern = "Supercalifragilisticexpialidocious";
+            foreach (char letter in supern)
+            {
+                Console.WriteLine(letter);
+            }
+        }
+        [TestMethod]
+        public void INotIandL()
+        {
+            string[] supern1 = { "S", "u", "p", "e", "r", "c", "a", "l", "i", "f", "r", "a", "g", "i", "l", "i", "s", "t", "i", "c", "e", "x", "p", "i", "a", "l", "i", "d", "o", "c", "i", "o", "u", "s" };
+
+            for (int index = 0; index < 34; index++)
+            {
                 if (supern1[index] == "i")
                 {
                     Console.WriteLine("i");
                 }
+                else
                 {
                     Console.WriteLine("Not an i");
                 }
-                    if (supern1[index] == "L")
-                        Console.WriteLine("L");
+                if (supern1[index] == "l")
+                {
+                    Console.WriteLine("L");
                 }
             }
+            int result = supern1.GetLength(0);
+            Console.WriteLine(result);
         }
     }
+}
 
 
 
